1ee9e7daed27319dcd12aeb05cc51205d8bf6bc9
Thur. <arthurpereiracardoso96@gmail.com>
2026-09-23 17:45:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -242,26 +242,3 @@ async function start() {
 }
 
 start();
-const PORT = process.env.PORT || 10000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain"
-  });
-
-  res.end("Bot Baguncinha online!");
-}).listen(PORT, "0.0.0.0", () => {
-  console.log(`🌐 Servidor HTTP rodando na porta ${PORT}`);
-});
-
-// =========================
-// INICIALIZAÇÃO
-// =========================
-
-async function start() {
-  await registerCommands();
-
-  client.login(TOKEN);
-}
-
-start();
