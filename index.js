b9636b33c021219d77baf18466f69cf9ff1d7177
Thur. <arthurpereiracardoso96@gmail.com>
2026-09-23 14:35:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const http = require("http");
 
 const client = new Client({
   intents: [
@@ -6,8 +7,18 @@ const client = new Client({
   ]
 });
 
+// Servidor HTTP necessário para o Render
+const PORT = process.env.PORT || 10000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot Baguncinha online!");
+}).listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor HTTP rodando na porta ${PORT}`);
+});
+
 client.once("ready", () => {
-  console.log(` Bot conectado como ${client.user.tag}`);
+  console.log(`Bot conectado como ${client.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
