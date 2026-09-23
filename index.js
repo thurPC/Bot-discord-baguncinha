07407463a3db4dab61eb30b9f6e9afdb92c8d267
Thur. <arthurpereiracardoso96@gmail.com>
2026-09-23 13:16:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
+
+client.once("ready", () => {
+  console.log(` Bot conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
